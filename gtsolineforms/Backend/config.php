812dbc9c711d19e69573f5f93b0c3fb3d149b03e
quaterpie3141 <86412584+Quarterpie3141@users.noreply.github.com>
2023-01-20 11:01:09 +0800

    Update config.php

--- gtsolineforms/Backend/config.php
+++ gtsolineforms/Backend/config.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "34.101.241.129";
-$username = "root";
-$password = "Quarterpie3141";
-$dbname = "ir_f_db";
+$servername = "************";
+$username = "***";
+$password = "********";
+$dbname = "********";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
