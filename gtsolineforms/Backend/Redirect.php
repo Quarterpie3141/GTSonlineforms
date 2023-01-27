5100a63e5e27db0007cd9d978adc3cda6488b384
quaterpie3141 <86412584+Quarterpie3141@users.noreply.github.com>
2023-01-27 09:19:36 +0800

    Delete gtsolineforms directory

--- gtsolineforms/Backend/Redirect.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: ../Forms/IR-F.html");
-    exit;
-} else{
-    header("location: ../Index.html");
-    exit();
-}
-?>
