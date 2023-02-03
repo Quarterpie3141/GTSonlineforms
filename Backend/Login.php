6504af58abfb999be4bc7723d723c4038d47b23f
Quarterpie3141 <prashan.csgo@gmail.com>
2023-02-03 15:12:13 +0800

    added a priv level to user lgns

--- Backend/Login.php
+++ Backend/Login.php
@@ -21,7 +21,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $password = trim($_POST["password"]);
     }
     if(empty($username_err) && empty($password_err)){
-        $sql = "SELECT id, username, password FROM usr_lgns WHERE username = ?";
+        $sql = "SELECT id, username, password, priv FROM usr_lgns WHERE username = ?";
         
         if($stmt = mysqli_prepare($conn, $sql)){
             mysqli_stmt_bind_param($stmt, "s", $param_username);
@@ -29,13 +29,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             if(mysqli_stmt_execute($stmt)){
                 mysqli_stmt_store_result($stmt);
                 if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password, $priv);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
                             session_start();
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
+                            $_SESSION["priv"] = $priv;
                             if(empty($URI)){
                                 header("location: ../pages/HomePage.php");
                             }else{
