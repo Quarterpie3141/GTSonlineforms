fd7d4155eb2874b0c84aeb2bda66262c86f956e5
Quarterpie3141 <prashan.csgo@gmail.com>
2023-02-09 15:27:50 +0800

    forgot mor dirs

--- Forms/IR-F/convert/IR-F.php
+++ Forms/IR-F/convert/IR-F.php
@@ -5,10 +5,10 @@
     <meta name="keywords" content="GoodTurnServices">
     <meta name="author" content="Prashan Wijesinghe">
     <meta name="viewport" content="width=device-width, initial-scale=0.2">
-    <link rel="stylesheet" href="../../css/Form.css">  
-    <link rel="icon" type="image/x-icon" href="../../Images/favicon.png"> 
-    <link href="bootstrap.min.css" rel="stylesheet">
-    <script src="../../js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../../../css/Form.css">  
+    <link rel="icon" type="image/x-icon" href="../../../Images/favicon.png"> 
+    <link href="../bootstrap.min.css" rel="stylesheet">
+    <script src="../../../js/bootstrap.bundle.min.js"></script>
     <title>IR-F-4</title>
 </head> 
 <?php
